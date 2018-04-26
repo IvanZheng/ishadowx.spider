02536e2c95b8a5f7e87c6ae71babc8bf671d427f
Ivan <yvonne_7_7@hotmail.com>
2018-04-27 00:21:12 +0800

    implement auto collect proxies

---
+++ IShadowx.Core/Source.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace IShadowx.Core
+{
+    public class Source
+    {
+        public string Url { get;set; }
+        public string Selector { get; set; }
+        public string IpSelector { get; set; }
+        public string PwdSelector { get; set; }
+        public string PortSelector { get; set; }
+        public string MethodSelector { get; set; }
+    }
+}
