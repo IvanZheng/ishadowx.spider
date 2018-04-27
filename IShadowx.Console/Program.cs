076aa8484a7fc5e721ebcf6a6f90fd5a3bc23126
Ivan Zheng <yvonne_7_7@hotmail.com>
2018-04-27 09:28:10 +0800

    improve appsettings

--- IShadowx.Console/Program.cs
+++ IShadowx.Console/Program.cs
@@ -18,43 +18,18 @@ namespace IShadowx.Console
             configurationBuilder.AddJsonFile(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "appsettings.json"), true, true);
             var configuration = configurationBuilder.Build();
             ConfigurationManager.UseConfiguration(configuration);
-            var sources = ConfigurationManager.Get<List<Source>>("sources");
-            var httpClient = new HttpClient();
-
-            var configs = new JArray();
-            foreach (var source in sources)
-            {
-                var htmlDocument = new HtmlAgilityPack.HtmlDocument();
-                var html = httpClient.GetAsync(source.Url).Result.Content.ReadAsStringAsync().Result;
-                htmlDocument.LoadHtml(html);
-                var nodes = htmlDocument.DocumentNode.SelectNodes(source.Selector);
-                foreach (var node in nodes)
-                {
-                    var ip = node.SelectSingleNode(source.IpSelector)?.InnerText.Trim();
-                    var port = node.SelectSingleNode(source.PortSelector)?.InnerText.Trim();
-                    var password = node.SelectSingleNode(source.PwdSelector)?.InnerText.Trim();
-                    var method = node.SelectSingleNode(source.MethodSelector)?.InnerText.Split(":")?.LastOrDefault()?.Trim();
-                    var config = new
-                    {
-                        server = ip,
-                        server_port = int.Parse(port),
-                        password,
-                        method,
-                        plugin = "",
-                        plugin_opts = "",
-                        remarks = "",
-                        timeout = 5
-                    };
-                    configs.Add(JsonConvert.DeserializeObject(JsonConvert.SerializeObject(config)));
-                }
-            }
-
-            var configFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "gui-config.json");
+            var sources = ConfigurationManager.Get<List<Source>>("sources")?
+                                              .ToArray();
+            var capturer = new Capturer();
+            var configs = capturer.ParseFromSource(sources);
+            var configFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, ConfigurationManager.Get("GuiConfigPath"));
             var guiConfigJson = File.ReadAllText(configFile);
             var guiConfig = JsonConvert.DeserializeObject<JObject>(guiConfigJson);
             guiConfig["configs"] = configs;
+
             File.WriteAllText(configFile, JsonConvert.SerializeObject(guiConfig));
-            System.Console.WriteLine("Hello world");
+
+            System.Console.WriteLine("Update Successful");
         }
     }
 }
---
+++ IShadowx.Core/Capturer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace IShadowx.Core
+{
+    public class Capturer
+    {
+        private static readonly HttpClient HttpClient = new HttpClient();
+
+        public JArray ParseFromSource(params Source[] sources)
+        {
+            var configs = new JArray();
+            foreach (var source in sources)
+            {
+                var htmlDocument = new HtmlAgilityPack.HtmlDocument();
+                var html = HttpClient.GetAsync(source.Url).Result.Content.ReadAsStringAsync().Result;
+                htmlDocument.LoadHtml(html);
+                var nodes = htmlDocument.DocumentNode.SelectNodes(source.Selector);
+                foreach (var node in nodes)
+                {
+                    var ip = node.SelectSingleNode(source.IpSelector)?.InnerText.Trim();
+                    var port = node.SelectSingleNode(source.PortSelector)?.InnerText.Trim();
+                    var password = node.SelectSingleNode(source.PwdSelector)?.InnerText.Trim();
+                    var method = node.SelectSingleNode(source.MethodSelector)?.InnerText.Split(':')?.LastOrDefault()?.Trim();
+                    var config = new
+                    {
+                        server = ip,
+                        server_port = int.Parse(port),
+                        password,
+                        method,
+                        plugin = "",
+                        plugin_opts = "",
+                        remarks = "",
+                        timeout = 5
+                    };
+                    configs.Add(JsonConvert.DeserializeObject(JsonConvert.SerializeObject(config)));
+                }
+            }
+            return configs;
+        }
+    }
+}
